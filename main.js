9c52d586ed55f7f794503dd032e5c8251aa16ba0
pedrodoni <doniquianp@gmail.com>
2023-06-08 17:30:26 -0300

    const Addcarrito

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ let alimentos = [
         id: 1,
         nombre: "Guaf Alimento balanceado 10 kg",
         precio: 3500,
-        imagen: "./images/guaf.png"
+        imagen: "../images/guaf.png"
     },
     {
         id: 2,
-        nombre: "Dog Chow alimento balanceado 10kg",
+        nombre: "Dog Chow alimento 10kg",
         precio: 6500,
-        imagen: "./images/dogChow.png"
+        imagen: "../images/dogChow.png"
     },
     {
         id: 3,
-        nombre: "Dog Selection alimento balanceado 10kg",
+        nombre: "Dog Selection alimento 10kg",
         precio: 7600,
-        imagen: "./images/dogSlection.png"
+        imagen: "../images/dogSelection.png"
     }
 ];
-const exhibir = document.querySelectorAll("produAlimentos")
+const exhibir = document.getElementById("produAlimentos")
 alimentos.forEach((alimento, indice) => { 
     let show = document.createElement("div")
     show.classList.add("GuafAlimento" , "AlimentosDiv")
@@ -26,9 +26,17 @@ alimentos.forEach((alimento, indice) => {
     ` <img src="${alimento.imagen}" alt="Guaf Alimento">
     <div>
     <p class="tituloAlimentos"> ${alimento.nombre}</p>
-    <p class="Precios"> ${alimento.precio}</p> 
-    <a href="#carrito" class="addCarrito" onClick="AddCarrito(${indice})>"añadir al carriyo"</a>
+    <p class="Precios">$${alimento.precio}</p> 
+    <a href="#carrito" class="btn btn-outline-light" onClick="AddCarrito(${indice})">Añadir al carrito</a>
     </div>`
     exhibir.appendChild(show)
   
 });
+
+const AddCarrito= ()=>{
+    Swal.fire(
+        'Buena decision!',
+        'Has añadido este producto a tu carrito!',
+        'success'
+    )
+}
