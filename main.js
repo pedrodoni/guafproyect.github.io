09ef6a44367d109db2deb4caf0a3311c87365ffe
pedrodoni <doniquianp@gmail.com>
2023-06-11 03:45:19 -0300

    repairs?

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+
 let carrito = []
+carrito = JSON.parse(localStorage.getItem('carrito'))
 const contieneProductos = document.getElementById("produAlimentos")
 const clearCart = document.getElementById("limpiar")
 const displayCarrito = document.getElementById("aparienciaCart")
 const total = document.getElementById("totalCart")
+const comprar = document.getElementById("alertEnvio")
 const alert= ()=>{
     Swal.fire(
         'Buena decision!',
@@ -10,12 +13,36 @@ const alert= ()=>{
         'success'
     )
 }
+comprar.addEventListener('click',()=>{
+    carrito.length = 0
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    freshCarrito()
+    Swal.fire({
+        title: 'Felicitaciones su pedido le llegara lo mas rapido posible!',
+        width: 600,
+        padding: '3em',
+        color: '#FD1D1D',
+      
+      })
+    
+})
 clearCart.addEventListener('click',()=>{
     carrito.length = 0
+    localStorage.setItem('carrito', JSON.stringify(carrito))
     freshCarrito()
+    Swal.fire({
+        title: 'Estas a tiempo!',
+        text: 'Preocupate por mi alimentacion',
+        imageUrl: 'https://cdn.dogsplanet.com/wp-content/uploads/2021/02/perro-triste.jpg',
+        imageWidth: 400,
+        imageHeight: 266,
+        imageAlt: 'Perrito Triste',
+      })
+    
 })
 
 alimentos.forEach((alimento)=>{
+    
     let alimentoBalanceado = document.createElement("div");
     alimentoBalanceado.setAttribute('id','AlimentosDiv')
     alimentoBalanceado.innerHTML=`<div class= "cardCarro">
@@ -34,6 +61,7 @@ alimentos.forEach((alimento)=>{
         addCarrito(alimento.id)
     }
     )
+    
 })
 const addCarrito = (alimentId) => {
     const D = alimentos.find((aliment)=> aliment.id === alimentId)
@@ -48,14 +76,17 @@ const freshCarrito=()=>{
 
 
     carrito.forEach((aliment)=>{
+
+
+        
         const alimentoBalanceado = document.createElement('div')
         alimentoBalanceado.setAttribute('id','aparienciaCarrito')
         alimentoBalanceado.innerHTML=`
-        <img src="${aliment.imagen}" alt="${aliment.nombre}">
-        <h3>${aliment.nombre}</h3>
-        <h4>${aliment.precio}</h4>
+        <img class="imageCart" src="${aliment.imagen}" alt="${aliment.nombre}">
+        <div class= "details"> <h3>${aliment.nombre}</h3>
+        <h4>$${aliment.precio}</h4> 
         <h5>Cantidad: ${aliment.cantidad}</h5>
-        <button onclick= "eliminar(${aliment.id})" class="delete"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-off" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6f32be" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <button onclick= "eliminar(${aliment.id})" class="delete"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-off" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M3 3l18 18" />
         <path d="M4 7h3m4 0h9" />
@@ -64,9 +95,11 @@ const freshCarrito=()=>{
         <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l.077 -.923" />
         <path d="M18.384 14.373l.616 -7.373" />
         <path d="M9 5v-1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-      </svg></button>`
+      </svg></button>
+      </div>`
 
       displayCarrito.appendChild(alimentoBalanceado)
+      localStorage.setItem('carrito', JSON.stringify(carrito))
     })
     total.innerText = carrito.reduce((acc, aliment)=> acc + aliment.precio, 0)
 }
@@ -75,5 +108,6 @@ const eliminar = (alimentID)=>{
     const index = carrito.indexOf(D)
     carrito.splice(index, 1)
     freshCarrito()
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
  
--- products.js
+++ products.js
@@ -1,21 +1,21 @@
 const alimentos = [
     {
         id: 1,
-        nombre: "Guaf Alimento balanceado 10 kg",
+        nombre: "Guaf 10 kg",
         precio: 3500,
         imagen: "../images/guaf.png",
         cantidad: 1
     },
     {
         id: 2,
-        nombre: "Dog Chow alimento 10kg",
+        nombre: "DogChow 10kg",
         precio: 6500,
         imagen: "../images/dogChow.png",
         cantidad: 1
     },
     {
         id: 3,
-        nombre: "Dog Selection alimento 10kg",
+        nombre: "Dog Selection 10kg",
         precio: 7600,
         imagen: "../images/dogSelection.png",
         cantidad: 1
